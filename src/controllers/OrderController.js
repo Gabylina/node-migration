f7f59d178401181a885bcd8f9917a17f2fb4ad3a
Gabylina <127333431+Gabylina@users.noreply.github.com>
2025-08-14 18:00:52 -0400

    Delete src/controllers directory

--- src/controllers/OrderController.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-// Migrated from OrderController.php
-router.get('/', (req,res)=>res.send('OrderController index'));
-router.post('/', (req,res)=>res.send('OrderController create'));
-module.exports = router;
--- src/controllers/ProductController.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-// Migrated from ProductController.php
-router.get('/', (req,res)=>res.send('ProductController index'));
-router.get('/detail/:id', (req,res)=>res.send('ProductController detail ' + req.params.id));
-module.exports = router;
