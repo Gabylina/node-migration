4a5ae067576dbfac408f8e58efd8c7f15dc69f81
Gabylina <127333431+Gabylina@users.noreply.github.com>
2025-08-14 18:00:23 -0400

    Delete src/controllers/NotificationController.js

--- src/controllers/NotificationController.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-// Migrated from NotificationController.php
-router.post('/send', (req,res)=>res.send('send'));
-router.post('/schedule', (req,res)=>res.send('schedule'));
-module.exports = router;
