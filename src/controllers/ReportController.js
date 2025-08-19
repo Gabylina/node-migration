9a75dd35af094759b521d16d072badacff9a6ad9
Gabylina <127333431+Gabylina@users.noreply.github.com>
2025-08-19 09:23:59 -0400

    Delete src/controllers directory

--- src/controllers/ReportController.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-// Migrated from ReportController.php
-router.get('/generate', (req,res)=>res.send('generate'));
-router.get('/download/:id', (req,res)=>res.send('download ' + req.params.id));
-module.exports = router;
--- src/controllers/UserController.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-// Migrated from UserController.php
-router.get('/', (req,res)=>res.send('UserController index'));
-router.get('/:id', (req,res)=>res.send('UserController show ' + req.params.id));
-module.exports = router;
